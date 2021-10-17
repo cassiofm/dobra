f093dfcc4f51396f5131122e616284f97465fe7d
cassiofm <cassiofm@bol.com.br>
2021-10-17 19:48:57 -0200

    desenvolvendo m1

--- maquina_dobra_v3/maquina_dobra_v3.ino
+++ maquina_dobra_v3/maquina_dobra_v3.ino
@@ -4,7 +4,7 @@
 
 //pinos de saida digital
 const int pulso_m1 = 4;
-const int direcao_m1 = 5;
+const int direcao_m1 = 12;
 const int pulso_m2 = 8;
 const int direcao_m2 = 9;
 
@@ -17,17 +17,25 @@ const int fim_curso2 = 6;
 
 //variaveis do sistema
 bool flag_barrilet = false;
+bool flag_m1 = false;
+bool flag_m2 = false;
 
 void setup() {
   // put your setup code here, to run once:
 
 pinMode(LED_BUILTIN, OUTPUT);
-
+pinMode(sensor_barrilet, INPUT);
+pinMode(pulso_m1, OUTPUT);
+pinMode(direcao_m1, OUTPUT);
+pinMode(pulso_m2, OUTPUT);
+pinMode(direcao_m2, OUTPUT);
+pinMode(fim_curso1, INPUT_PULLUP);
+pinMode(fim_curso2, INPUT_PULLUP);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
- if(flag_barrilet == false) {home_barrilet();}
+ /*if(flag_barrilet == false) {home_barrilet();}
 
  else{
     for(int i=0; i<1580; i++){
@@ -39,7 +47,9 @@ void loop() {
       passof();
       }
     delay(2000);
-  }
+  }  */
+ // if(flag_m1 == false){home_m1();}
+ home_m1();
 }
 
 
@@ -65,3 +75,20 @@ void home_barrilet() {
   flag_barrilet = true;
   delay(2000);
 }
+
+void home_m1(){
+  while(digitalRead(fim_curso1) == HIGH){
+    digitalWrite(direcao_m1, HIGH);
+    passo_m1();
+    }
+    flag_m1 = true;
+  }
+
+  void passo_m1(){
+  
+  digitalWrite(pulso_m1, HIGH);
+  delayMicroseconds(3000);
+  digitalWrite(pulso_m1,LOW);
+  delayMicroseconds(3000);
+  }
+  
